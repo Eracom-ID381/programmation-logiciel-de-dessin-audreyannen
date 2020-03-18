462227c0dd742fb45bd1864cfa2f03581cf373b1
audreyannen <60931133+audreyannen@users.noreply.github.com>
2020-03-18 15:42:29 +0100

    sound

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,39 @@
+let osc, freq, amp;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(0);
+    osc = new p5.Oscillator('triangle');
 }
 
 function draw() {
     rcolor = color(random(200, 255), random(0, 200), random(0, 255));
 
     stroke(rcolor);
+    freq = map(mouseX, 0, width, 100, 500);
+    amp = map(mouseY, 0, height, 0, 1);
     if (mouseIsPressed === true) {
         line(mouseX, mouseY, 0, 0);
+        osc.freq(freq, 0.1);
+        osc.amp(amp, 0.1);
     }
+}
+
+function mousePressed() {
+    osc.start();
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     background(0);
+}
+
+function keyPressed() {
+    if (keyCode === BACKSPACE) {
+        background(0);
+    }
+}
+
+function mouseReleased() {
+    osc.stop();
 }
